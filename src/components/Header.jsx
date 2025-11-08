bbad4f8c2f32f029b2a3b4aab13468d436aea8b2
Miguel Mateos <miguelmateosruiz@gmail.com>
2025-11-08 17:55:04 +0100

    Modificar header para añadir enlace condicional segun en la pagina que esté

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,17 @@
 import '../styles/header.css'
+import { Link } from 'react-router-dom'
 
 export function Header({ page }) {
+
+    const linkPage = page === 'Crear Producto' ? '/' : '/producto/nuevo'
+    const linkName = page === 'Crear Producto' ? 'Volver al Dashboard' : '+ Crear Producto'
 
     return(
         <header>
             <img src="/logo_app.png" alt="Logo de la cafeteria, taza de cafe combiando con el simbolo de React"/>
             <h1>Café & Sabor - {page}</h1>
+
+            <Link to={linkPage}>{linkName}</Link>
 
         </header>
 
