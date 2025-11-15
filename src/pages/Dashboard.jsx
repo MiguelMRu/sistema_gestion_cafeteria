d46d6f04a6df9743bf5c4625ba60ab75eb686a58
Miguel Mateos <miguelmateosruiz@gmail.com>
2025-11-15 18:35:28 +0100

    añadir componente Filters

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,11 +1,13 @@
 import { Header } from "../components/Header"
 import { Products } from "../components/Products"
+import { Filters } from "../components/Filters"
 export default function Dashboard() {
 
 
   return (
         <>
           <Header page="Lista de productos" />
+          <Filters/>
           <Products />
         </>
   )
