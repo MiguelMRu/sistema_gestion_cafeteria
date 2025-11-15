8866e97d0959695e715a263b879bfa243162ed8d
Miguel Mateos <miguelmateosruiz@gmail.com>
2025-11-15 18:34:08 +0100

    Crear componente Filter Select

---
+++ src/components/FilterSelect.jsx
@@ -0,0 +1,18 @@
+
+
+export function FilterSelect({ options, value, onChange }) {
+
+    return (
+        <select value={value} onChange={onChange}>
+            <option value="all">Todos</option>
+            {options.map((option, index) => (
+                <option 
+                    key={index} 
+                    value={option.value}
+                >
+                    {option.label}
+                </option>
+            ))}
+        </select>
+    )
+}
